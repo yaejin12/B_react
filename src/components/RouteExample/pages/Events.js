fdf647e92cb139176fbda115ab61103ebcf6b341
yaejin12 <pos03167@naver.com>
2024-07-03 12:17:42 +0900

    fetch

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -7,6 +7,7 @@ const Events = () => {
   const eventList = useLoaderData();
   // console.log(eventList);
 
+  console.log("event page rendering!");
   return (
     <>
       <h1>Events Page</h1>
@@ -17,19 +18,29 @@ const Events = () => {
 
 export default Events;
 
-//loader를 app.js로 부터 아웃소싱
+// loader를 app.js로부터 아웃소싱
+
+// export const loader = async () => fetch('http://localhost:8282/events');
+
 export const loader = async () => {
+  console.log("loader call!");
+
   // 이 페이지가 열릴때 자동으로 트리거되어 호출되는 함수
   // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
 
-  // console.log('loader call!!');
-  const response = await fetch("http://localhost:8282/events");
+  const response = await fetch("http://localhost:8282/events?sort=date");
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Response(JSON.stringify({ message: errorText }), {
+      status: response.status,
+    }); //코드 흐름의 끊어줘야한다
+  }
   // const jsonData = await response.json();
 
   // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서
   // 언제든 불러서 사용할 수 있다.
-  // console.log(jsonData);
 
-  //loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌
-  return response;
+  // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌.
+  return response; //ok일 경우 events[]
 };
